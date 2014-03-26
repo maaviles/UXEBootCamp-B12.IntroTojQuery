a321db9eeb1fe837470380c32a80ceab92937ccb
maaviles <macyaviles@MM.local>
2014-03-26 01:13:16 -0700

    added more to jQuery file, just need to add traversing and add/edit dom elements

--- Macy/javascripts/app.js
+++ Macy/javascripts/app.js
@@ -1,5 +1,35 @@
 jQuery(document).ready(function() {
 
-// start code here
+// Things I will show:
+// Find and modify nodes in the DOM tree
+// Use various CSS selectors as jQuery selectors to gram nodes in the DOM
 
-}):
+  // element selector
+
+$("li").text("redrum o__o");
+
+  // ID selector
+
+$("#hireMe").hover(
+  function() {
+    var $this = $(this);
+    $this.data("HIRE ME", $this.text());
+    $this.text("HIRED!!!1!");
+  },
+  function () {
+    var $this = $(this);
+    $this.text($this.data("HIRE ME"));
+  }
+);
+
+  // Class selector
+
+$(".scale").hover(function() {
+  $("#hidden_div").show();
+}).mouseout(function() { 
+  $("#hidden_div").hide();
+});
+
+// Traverse the DOM and add/edit DOM elements
+
+});
