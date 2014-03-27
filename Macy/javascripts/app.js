1519ebd0139cf0fdd67dac3b639c0bc593f348d6
maaviles <macyaviles@MM.local>
2014-03-26 22:52:09 -0700

    added simple button and practiced more DOM traversing and manipulating

--- Macy/javascripts/app.js
+++ Macy/javascripts/app.js
@@ -6,52 +6,74 @@ jQuery(document).ready(function() {
 
   // element selector
 
-$("span").text("redrum o__o");
+  $("span").text("redrum o__o");
 
   // ID selector
+  // use of $(this) keyword
 
-$("#hireMe").hover(
-  function() {
-    var $this = $(this);
-    $this.data("hireMe", $this.text());
-    $this.text("HIRED!!!1!");
-  },
-  function () {
-    var $this = $(this);
-    $this.text($this.data("hireMe"));
-  }
-);
+  $("#hireMe").hover(
+    function() {
+      var $this = $(this);
+      $this.data("hireMe", $this.text());
+      $this.text("HIRED!!!1!");
+    },
+    function () {
+      var $this = $(this);
+      $this.text($this.data("hireMe"));
+    }
+  );
 
   // Class selector
 
-$(".scale").hover(function() {
-  $("#hidden_div").show();
-}).mouseout(function() { 
-  $("#hidden_div").hide();
-});
+  $(".scale").hover(function() {
+    $("#hidden_div").show();
+  }).mouseout(function() { 
+    $("#hidden_div").hide();
+  });
 
-// Traverse the DOM and add/edit DOM elements
 
   // Descendant selector
 
-$("#welcome h1").text("Built with Meeps");
+  $("#welcome h1").text("Built with Meeps");
 
   // Select only direct children
 // need to fix
 //$("#meep > li").text("Booop");
 
   // pseudo classes
-$("#meep li:odd").text("I am so odd");
+  $("#meep li:odd").text("I am so odd");
 
-  // Traversing the Dom
+
   // this is a faster alternative to other selectors:
-$("#meep").find("li");
+  $("#meep").find("li");
 
-$("h2").last().text("here I am!");
+  // traveling up and down the DOM
+  $("h2").last().text("here I am!");
+  $("li").first().next().prev().text("no, here I am!");
 
-$("li").first().next().prev().text("no, here I am!");
+  //  appending to the DOM
+//var price = $("<p>the price</p>");
+//$(".vacation").append(price);
+  //  removing from the DOM
+//$("button").remove();
 
+  //  watching for click
+    //  jQuery object methods:
+      //  .on(<event>, <event handler>)
+  $(".vacation").on("click", "button", function () {
 
-// keyboard events, link layover
+  // this will run when the button is clicked
+
+    //var price = $("<p>the price</p>");
+    //replaced for future buttons
+    var vacation = $(this).closest(".vacation");
+    var amount = vacation.data("price");
+    var price = $("<p>$" + amount + "</p>");
+
+    // put this instead of .after() in case things move around
+    vacation.append(price);
+    $(this).remove();
+
+  });
 
 });
